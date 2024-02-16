cbda63599232c42cd2b4bdb7ff46c12f420202c1
Andre Izarra <programador385@gmail.com>
2024-02-15 21:13:57 -0400

    :bug: fix subscriptions

--- src/modules/league/subscriptions/index.ts
+++ src/modules/league/subscriptions/index.ts
@@ -1,10 +1,15 @@
 import { finishClash } from "../useCases/finishClash";
+import { sendNotifications } from "../useCases/sendNotifications";
 import { updateTeamRanking } from "../useCases/updateTeamRanking";
 import { updateTeamStats } from "../useCases/updateTeamStats";
+import { AfterAdCreated } from "./afterAdCreated";
 import { AfterClashFinish } from "./afterClashFinished";
+import { AfterClubEventCreated } from "./afterClubEventCreated";
 import { AfterMatchCancelled } from "./afterMatchCancelled";
 import { AfterMatchFinished } from "./afterMatchFinished";
 
 new AfterClashFinish(updateTeamStats, updateTeamRanking);
 new AfterMatchFinished(finishClash);
 new AfterMatchCancelled(finishClash);
+new AfterAdCreated(sendNotifications)
+new AfterClubEventCreated(sendNotifications)
