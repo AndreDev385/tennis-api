3841f5e7dd6cccece2968dd76dfd10cb519685fc
Andre Izarra <programador385@gmail.com>
2024-03-18 09:07:06 -0400

    :construction: notifications

--- public/admin/src/env/env.prod.ts
+++ public/admin/src/env/env.prod.ts
@@ -1,2 +1,2 @@
-//export const VITE_SERVER_URL = "https://gamemind-app-test-73231707e870.herokuapp.com";
-export const VITE_SERVER_URL = "https://gamemind-app-7ce0a0ff4c64.herokuapp.com"
+export const VITE_SERVER_URL = "https://gamemind-app-test-73231707e870.herokuapp.com";
+//export const VITE_SERVER_URL = "https://gamemind-app-7ce0a0ff4c64.herokuapp.com"
