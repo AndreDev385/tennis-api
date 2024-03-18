9012a30de2f1665e496b763e0bb98e26058bcc14
Andre Izarra <programador385@gmail.com>
2024-03-18 15:03:35 -0400

    :recycle: :bug: refactor team stats calculation

--- src/modules/league/useCases/updateTeamStats/updateTeamStats.ts
+++ src/modules/league/useCases/updateTeamStats/updateTeamStats.ts
@@ -2,7 +2,7 @@ import { AppError } from "../../../../shared/core/AppError";
 import { Either, Result, left, right } from "../../../../shared/core/Result";
 import { UseCase } from "../../../../shared/core/UseCase";
 import { Clash } from "../../domain/clubClash";
-import { TeamStats } from "../../domain/teamStats";
+import { TeamStats, calculateTeamStats } from "../../domain/teamStats";
 import { ClashRepository } from "../../repositories/clashRepo";
 import { TeamStatsRepository } from "../../repositories/teamStatsRepo";
 import { UpdateTeamStatsRequest } from "./dtos";
@@ -16,12 +16,16 @@ export class UpdateTeamStats implements UseCase<any, any> {
     private teamStatsRepo: TeamStatsRepository;
     private clashRepo: ClashRepository;
 
-    constructor(teamStatsRepo: TeamStatsRepository, clashRepo: ClashRepository) {
+    constructor(
+        teamStatsRepo: TeamStatsRepository,
+        clashRepo: ClashRepository
+    ) {
         this.clashRepo = clashRepo;
         this.teamStatsRepo = teamStatsRepo;
     }
 
     async execute(req: UpdateTeamStatsRequest): Promise<Response> {
+        let clashes: Array<Clash>;
         let clash: Clash;
         let teamStats: TeamStats;
         try {
@@ -30,6 +34,12 @@ export class UpdateTeamStats implements UseCase<any, any> {
             } catch (error) {
                 return left(new AppError.NotFoundError(error));
             }
+
+            clashes = await this.clashRepo.list({
+                team1: clash.team1.id.toString(),
+                seasonId: clash.seasonId.id.toString(),
+            });
+
             try {
                 teamStats = await this.teamStatsRepo.getStats(
                     clash.seasonId.id.toString(),
@@ -44,7 +54,9 @@ export class UpdateTeamStats implements UseCase<any, any> {
                 );
             }
 
-            teamStats.addClashStats(clash);
+            const stats = calculateTeamStats(clashes);
+
+            teamStats.addTeamStats(stats);
 
             await this.teamStatsRepo.save(teamStats);
 
