e86ca44588501291d1b56367674b8443111b3570
Andre Izarra <programador385@gmail.com>
2024-07-05 17:54:21 -0400

    :sparkles: add status filter to get tournament matches

--- src/modules/tournaments/repository/tournamentMatchRepo.ts
+++ src/modules/tournaments/repository/tournamentMatchRepo.ts
@@ -1,21 +1,22 @@
 import { Result } from "../../../shared/core/Result";
 import {
-    PaginateQuery,
-    PaginateResponse,
+	PaginateQuery,
+	PaginateResponse,
 } from "../../../shared/infra/database/sequelize/queries/sequelizeQueries";
 import { TournamentMatch } from "../domain/tournamentMatch";
 
 export type TournamentMatchQuery = {
-    matchId?: string | string[];
-    tournamentId?: string;
-    contestId?: string;
+	matchId?: string | string[];
+	tournamentId?: string;
+	contestId?: string;
+	status?: number;
 };
 
 export type TournamentMatchRepo = {
-    paginate(
-        q: TournamentMatchQuery,
-        p: PaginateQuery
-    ): Promise<PaginateResponse<TournamentMatch>>;
-    get(q: TournamentMatchQuery): Promise<Result<TournamentMatch>>;
-    save(match: TournamentMatch): Promise<void>;
+	paginate(
+		q: TournamentMatchQuery,
+		p: PaginateQuery,
+	): Promise<PaginateResponse<TournamentMatch>>;
+	get(q: TournamentMatchQuery): Promise<Result<TournamentMatch>>;
+	save(match: TournamentMatch): Promise<void>;
 };
