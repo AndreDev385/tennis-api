85c59c58bea5868a9591a971e8eb3336c0d8f4f6
Andre Izarra <programador385@gmail.com>
2024-09-10 15:04:21 -0400

    :contruction: check for invalid date

--- src/modules/tournaments/usecase/newTournament/newTournamentController.ts
+++ src/modules/tournaments/usecase/newTournament/newTournamentController.ts
@@ -5,61 +5,60 @@ import { AppError } from "../../../../shared/core/AppError";
 import { Result } from "../../../../shared/core/Result";
 
 export class NewTournamentController extends BaseController {
-    private readonly usecase: NewTournament;
+	private readonly usecase: NewTournament;
 
-    constructor(usecase: NewTournament) {
-        super();
-        this.usecase = usecase;
-    }
+	constructor(usecase: NewTournament) {
+		super();
+		this.usecase = usecase;
+	}
 
-    async executeImpl(req: Request, res: Response) {
-        const {
-            name,
-            gamesPerSet,
-            setsQuantity,
-            matchesPerClash,
-            goldenPoint,
-            startDate,
-            endDate,
-            address,
+	async executeImpl(req: Request, res: Response) {
+		const {
+			name,
+			gamesPerSet,
+			setsQuantity,
+			matchesPerClash,
+			goldenPoint,
+			startDate,
+			endDate,
+			address,
+		} = req.body;
 
-        } = req.body;
+		console.log(req.body);
 
-        const result = await this.usecase.execute({
-            name,
-            goldenPoint: JSON.parse(goldenPoint),
-            gamesPerSet: Number(gamesPerSet),
-            setsQuantity: Number(setsQuantity),
-            matchesPerClash: Number(matchesPerClash) as any,
-            address,
-            startDate,
-            endDate,
-            file: req.file,
-        });
+		const result = await this.usecase.execute({
+			name,
+			goldenPoint: JSON.parse(goldenPoint),
+			gamesPerSet: Number(gamesPerSet),
+			setsQuantity: Number(setsQuantity),
+			matchesPerClash: Number(matchesPerClash) as any,
+			address,
+			startDate,
+			endDate,
+			file: req.file,
+		});
 
-        if (result.isLeft()) {
-            const error = result.value;
-            switch (error.constructor) {
-                case AppError.UnexpectedError:
-                    return this.fail(
-                        res,
-                        (error as AppError.UnexpectedError).getErrorValue()
-                            .message
-                    );
-                case AppError.NotFoundError:
-                    return this.clientError(
-                        res,
-                        (error as AppError.UnexpectedError).getErrorValue()
-                            .message
-                    );
-                default:
-                    return this.clientError(
-                        res,
-                        (error as Result<string>).getErrorValue()
-                    );
-            }
-        }
+		if (result.isLeft()) {
+			const error = result.value;
+			switch (error.constructor) {
+				case AppError.UnexpectedError:
+					return this.fail(
+						res,
+						(error as AppError.UnexpectedError).getErrorValue().message,
+					);
+				case AppError.NotFoundError:
+					return this.clientError(
+						res,
+						(error as AppError.UnexpectedError).getErrorValue().message,
+					);
+				default:
+					return this.clientError(
+						res,
+						(error as Result<string>).getErrorValue(),
+					);
+			}
+		}
 
-        return this.ok(res, { message: "Nuevo torneo agregado" });
-    }
+		return this.ok(res, { message: "Nuevo torneo agregado" });
+	}
 }
