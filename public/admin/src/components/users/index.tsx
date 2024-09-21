45c13cb96b8b5523369f08fb1e4ed2a946eeabfb
Andre Izarra <programador385@gmail.com>
2024-09-21 11:41:33 -0400

    :bug: create user :feat: filter users with ci

--- public/admin/src/components/users/index.tsx
+++ public/admin/src/components/users/index.tsx
@@ -1,20 +1,20 @@
 import {
-	faCheckCircle,
-	faCircle,
-	faCircleNotch,
-	faEllipsisVertical,
-	faSearch,
-	faUser,
+  faCheckCircle,
+  faCircle,
+  faCircleNotch,
+  faEllipsisVertical,
+  faSearch,
+  faUser,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
 import {
-	Button,
-	Card,
-	Dropdown,
-	Form,
-	InputGroup,
-	Table,
+  Button,
+  Card,
+  Dropdown,
+  Form,
+  InputGroup,
+  Table,
 } from "react-bootstrap";
 import { ToastContainer, toast } from "react-toastify";
 
@@ -25,245 +25,246 @@ import ModalQuestion from "../modalQuestion/ModalQuestion";
 import { deleteUser } from "../../services/user/deleteUser";
 
 type ComponentState = {
-	loading: boolean;
-	error: string;
+  loading: boolean;
+  error: string;
 };
 
 export function UsersTablePage() {
-	const [state, setState] = useState<ComponentState>({
-		loading: true,
-		error: "",
-	});
-	const [search, setSearch] = useState<string>("");
-	const [users, setUsers] = useState<User[]>([]);
-	const [filteredUsers, setFilteredUsers] = useState<User[]>([]);
-	const token: string = localStorage.getItem("authorization") || "";
+  const [state, setState] = useState<ComponentState>({
+    loading: true,
+    error: "",
+  });
+  const [search, setSearch] = useState<string>("");
+  const [users, setUsers] = useState<User[]>([]);
+  const [filteredUsers, setFilteredUsers] = useState<User[]>([]);
+  const token: string = localStorage.getItem("authorization") || "";
 
-	const [selectedUser, setSelectedUser] = useState<User | null>(null);
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
 
-	const [editUserModal, setEditUserModal] = useState<{ visible: boolean }>({
-		visible: false,
-	});
-	const [removeUserModal, setRemoveUserModal] = useState<{ visible: boolean }>({
-		visible: false,
-	});
+  const [editUserModal, setEditUserModal] = useState<{ visible: boolean }>({
+    visible: false,
+  });
+  const [removeUserModal, setRemoveUserModal] = useState<{ visible: boolean }>({
+    visible: false,
+  });
 
-	const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-		e.preventDefault();
-		setSearch(e.target.value);
-	};
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+  };
 
-	const handleListUsers = async () => {
-		setState({ loading: true, error: "" });
-		const result = await listUsers({
-			token,
-			query: { isAdmin: "false", canTrack: "false" },
-		});
+  const handleListUsers = async () => {
+    setState({ loading: true, error: "" });
+    const result = await listUsers({
+      token,
+      query: { isAdmin: "false", canTrack: "false" },
+    });
 
-		if (result.isFailure) {
-			setState({ loading: false, error: result.getErrorValue()! });
-			return;
-		}
+    if (result.isFailure) {
+      setState({ loading: false, error: result.getErrorValue()! });
+      return;
+    }
 
-		setState({ loading: false, error: "" });
-		setUsers(result.getValue());
-	};
+    setState({ loading: false, error: "" });
+    setUsers(result.getValue());
+  };
 
-	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
-	useEffect(() => {
-		handleListUsers();
-	}, []);
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  useEffect(() => {
+    handleListUsers();
+  }, []);
 
-	useEffect(() => {
-		const delayDebounceFn = setTimeout(() => {
-			setFilteredUsers(
-				users.filter(
-					(item) =>
-						item.firstName.toUpperCase().includes(search.toUpperCase()) ||
-						item.lastName.toUpperCase().includes(search.toUpperCase()) ||
-						item.email?.toUpperCase().includes(search.toUpperCase()),
-				),
-			);
-		}, 1000);
-		return () => clearTimeout(delayDebounceFn);
-	}, [search, users]);
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      setFilteredUsers(
+        users.filter(
+          (item) =>
+            item.firstName.toUpperCase().includes(search.toUpperCase()) ||
+            item.lastName.toUpperCase().includes(search.toUpperCase()) ||
+            item.ci?.toUpperCase().includes(search.toUpperCase()) ||
+            item.email?.toUpperCase().includes(search.toUpperCase()),
+        ),
+      );
+    }, 1000);
+    return () => clearTimeout(delayDebounceFn);
+  }, [search, users]);
 
-	const handleRemoveUser = async () => {
-		if (!selectedUser) {
-			toast.error("Error al seleccionar usuario");
-			return;
-		}
+  const handleRemoveUser = async () => {
+    if (!selectedUser) {
+      toast.error("Error al seleccionar usuario");
+      return;
+    }
 
-		setState({ loading: true, error: "" });
+    setState({ loading: true, error: "" });
 
-		const result = await deleteUser({
-			userId: selectedUser.userId,
-			token,
-		});
+    const result = await deleteUser({
+      userId: selectedUser.userId,
+      token,
+    });
 
-		await handleListUsers();
+    await handleListUsers();
 
-		setState({ loading: false, error: "" });
+    setState({ loading: false, error: "" });
 
-		if (result.isFailure) {
-			toast.error(result.getErrorValue());
-			return;
-		}
+    if (result.isFailure) {
+      toast.error(result.getErrorValue());
+      return;
+    }
 
-		toast.success(result.getValue());
-	};
+    toast.success(result.getValue());
+  };
 
-	const dismissModal = (loadUsers: boolean) => {
-		setEditUserModal({ visible: false });
-		if (loadUsers) {
-			handleListUsers();
-		}
-	};
+  const dismissModal = (loadUsers: boolean) => {
+    setEditUserModal({ visible: false });
+    if (loadUsers) {
+      handleListUsers();
+    }
+  };
 
-	const table = filteredUsers.map((item: User) => {
-		return (
-			<tr key={item.userId}>
-				<td className="text-center">
-					{item.firstName} {item.lastName}
-				</td>
-				<td className="text-center">{item.email}</td>
-				<td className="text-center">{item.ci}</td>
-				<td className="text-center">
-					{item.isDeleted ? (
-						<FontAwesomeIcon
-							style={{ color: "#fb7676" }}
-							className="circle"
-							icon={faCircle}
-						/>
-					) : (
-						<FontAwesomeIcon
-							style={{ color: "#0fb85b" }}
-							className="check-circle"
-							icon={faCheckCircle}
-						/>
-					)}
-				</td>
-				<td className="text-center">
-					<Dropdown>
-						<Dropdown.Toggle
-							color="blue"
-							as={Button}
-							id="dropdown-basic"
-							variant="link"
-						>
-							<FontAwesomeIcon
-								color="black"
-								className="ellipsis"
-								icon={faEllipsisVertical}
-							/>
-						</Dropdown.Toggle>
-						<Dropdown.Menu>
-							<Dropdown.Item
-								disabled={item.isDeleted}
-								onMouseDown={() => {
-									setSelectedUser(item);
-									setEditUserModal({ visible: true });
-								}}
-							>
-								Editar
-							</Dropdown.Item>
-							<Dropdown.Item
-								disabled={item.isDeleted}
-								onMouseDown={() => {
-									setSelectedUser(item);
-									setRemoveUserModal({ visible: true });
-								}}
-							>
-								Eliminar
-							</Dropdown.Item>
-						</Dropdown.Menu>
-					</Dropdown>
-				</td>
-			</tr>
-		);
-	});
+  const table = filteredUsers.map((item: User) => {
+    return (
+      <tr key={item.userId}>
+        <td className="text-center">
+          {item.firstName} {item.lastName}
+        </td>
+        <td className="text-center">{item.email}</td>
+        <td className="text-center">{item.ci}</td>
+        <td className="text-center">
+          {item.isDeleted ? (
+            <FontAwesomeIcon
+              style={{ color: "#fb7676" }}
+              className="circle"
+              icon={faCircle}
+            />
+          ) : (
+            <FontAwesomeIcon
+              style={{ color: "#0fb85b" }}
+              className="check-circle"
+              icon={faCheckCircle}
+            />
+          )}
+        </td>
+        <td className="text-center">
+          <Dropdown>
+            <Dropdown.Toggle
+              color="blue"
+              as={Button}
+              id="dropdown-basic"
+              variant="link"
+            >
+              <FontAwesomeIcon
+                color="black"
+                className="ellipsis"
+                icon={faEllipsisVertical}
+              />
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item
+                disabled={item.isDeleted}
+                onMouseDown={() => {
+                  setSelectedUser(item);
+                  setEditUserModal({ visible: true });
+                }}
+              >
+                Editar
+              </Dropdown.Item>
+              <Dropdown.Item
+                disabled={item.isDeleted}
+                onMouseDown={() => {
+                  setSelectedUser(item);
+                  setRemoveUserModal({ visible: true });
+                }}
+              >
+                Eliminar
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </td>
+      </tr>
+    );
+  });
 
-	function render() {
-		if (state.loading) {
-			return (
-				<FontAwesomeIcon className="center mt-5" icon={faCircleNotch} spin />
-			);
-		}
-		if (state.error.length > 0) {
-			return state.error;
-		}
-		return table;
-	}
+  function render() {
+    if (state.loading) {
+      return (
+        <FontAwesomeIcon className="center mt-5" icon={faCircleNotch} spin />
+      );
+    }
+    if (state.error.length > 0) {
+      return state.error;
+    }
+    return table;
+  }
 
-	return (
-		<>
-			{editUserModal.visible && (
-				<EditUserModal user={selectedUser!} dismiss={dismissModal} />
-			)}
-			{removeUserModal.visible && (
-				<ModalQuestion
-					title="Eliminar"
-					question={`¿Estás seguro que quieres eliminar a ${selectedUser?.firstName} ${selectedUser?.lastName}`}
-					dismiss={() => {
-						setSelectedUser(null);
-						setRemoveUserModal({ visible: false });
-					}}
-					accept={() => handleRemoveUser()}
-				/>
-			)}
-			<div className="admins-container">
-				<div className="title-wrap">
-					<h1>
-						<FontAwesomeIcon icon={faUser} />
-						Usuaios
-					</h1>
-				</div>
+  return (
+    <>
+      {editUserModal.visible && (
+        <EditUserModal user={selectedUser!} dismiss={dismissModal} />
+      )}
+      {removeUserModal.visible && (
+        <ModalQuestion
+          title="Eliminar"
+          question={`¿Estás seguro que quieres eliminar a ${selectedUser?.firstName} ${selectedUser?.lastName}`}
+          dismiss={() => {
+            setSelectedUser(null);
+            setRemoveUserModal({ visible: false });
+          }}
+          accept={() => handleRemoveUser()}
+        />
+      )}
+      <div className="admins-container">
+        <div className="title-wrap">
+          <h1>
+            <FontAwesomeIcon icon={faUser} />
+            Usuaios
+          </h1>
+        </div>
 
-				<div className="filter-container">
-					<InputGroup className="search">
-						<InputGroup.Text id="searchBar">
-							<FontAwesomeIcon icon={faSearch} />
-						</InputGroup.Text>
-						<Form.Control
-							placeholder="Buscar..."
-							aria-label="search"
-							aria-describedby="searchBar"
-							className="input-search"
-							value={search}
-							onChange={onChangeSearch}
-						/>
-					</InputGroup>
-				</div>
+        <div className="filter-container">
+          <InputGroup className="search">
+            <InputGroup.Text id="searchBar">
+              <FontAwesomeIcon icon={faSearch} />
+            </InputGroup.Text>
+            <Form.Control
+              placeholder="Buscar..."
+              aria-label="search"
+              aria-describedby="searchBar"
+              className="input-search"
+              value={search}
+              onChange={onChangeSearch}
+            />
+          </InputGroup>
+        </div>
 
-				<Card>
-					<Table responsive="sm">
-						<thead>
-							<tr>
-								<th className="text-center">Nombre y Apellido</th>
-								<th className="text-center">Email</th>
-								<th className="text-center">CI</th>
-								<th className="text-center">Activo</th>
-								<th className="text-center">Acciones</th>
-							</tr>
-						</thead>
+        <Card>
+          <Table responsive="sm">
+            <thead>
+              <tr>
+                <th className="text-center">Nombre y Apellido</th>
+                <th className="text-center">Email</th>
+                <th className="text-center">CI</th>
+                <th className="text-center">Activo</th>
+                <th className="text-center">Acciones</th>
+              </tr>
+            </thead>
 
-						<tbody>{render()}</tbody>
-					</Table>
-				</Card>
+            <tbody>{render()}</tbody>
+          </Table>
+        </Card>
 
-				<ToastContainer
-					position="top-right"
-					autoClose={5000}
-					hideProgressBar={false}
-					newestOnTop={false}
-					closeOnClick
-					rtl={false}
-					pauseOnFocusLoss
-					draggable
-					pauseOnHover
-					theme="light"
-				/>
-			</div>
-		</>
-	);
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+      </div>
+    </>
+  );
 }
--- src/modules/users/useCases/createUser/createUserUseCase.ts
+++ src/modules/users/useCases/createUser/createUserUseCase.ts
@@ -11,159 +11,160 @@ import { CreateUserErrors } from "./createUserErrors";
 import { CI } from "../../domain/ci";
 
 type Response = Either<
-    | CreateUserErrors.EmailAlreadyExistsError
-    | AppError.UnexpectedError
-    | Result<string>,
-    Result<void>
+  | CreateUserErrors.EmailAlreadyExistsError
+  | AppError.UnexpectedError
+  | Result<string>,
+  Result<void>
 >;
 
 type StandartUserData = {
-    firstName: Name,
-    lastName: Name,
-    email: UserEmail,
-    password: UserPassword,
-}
+  firstName: Name;
+  lastName: Name;
+  email: UserEmail;
+  password: UserPassword;
+};
 
 type CreateTracker = StandartUserData & {
-    canTrack: boolean,
-}
+  canTrack: boolean;
+};
 
 type UserData = StandartUserData & {
-    ci: CI,
-}
+  ci: CI;
+};
 
 export class CreateUserUseCase
-    implements UseCase<CreateUserDto, Promise<Response>>
+  implements UseCase<CreateUserDto, Promise<Response>>
 {
-    private repository: UserRepository;
+  private repository: UserRepository;
 
-    constructor(userRepository: UserRepository) {
-        this.repository = userRepository;
+  constructor(userRepository: UserRepository) {
+    this.repository = userRepository;
+  }
+
+  private async createUser(
+    data: UserData,
+  ): Promise<Either<Result<string>, Result<void>>> {
+    try {
+      const userRegisteredByAdmin = await this.repository.get({
+        ci: data.ci.value,
+      });
+
+      if (!!userRegisteredByAdmin.email) {
+        return left(
+          Result.fail<string>(
+            `CI: ${data.ci.value} ya se encuentra registrada`,
+          ),
+        );
+      }
+      // update user
+      userRegisteredByAdmin.editUser(
+        userRegisteredByAdmin.firstName,
+        userRegisteredByAdmin.lastName,
+        data.email,
+        data.ci,
+      );
+
+      userRegisteredByAdmin.changePassword(data.password);
+
+      await this.repository.save(userRegisteredByAdmin);
+      return right(Result.ok<void>());
+    } catch (e) {}
+
+    const userOrError = User.create({
+      firstName: data.firstName,
+      lastName: data.lastName,
+      ci: data.ci,
+      email: data.email,
+      password: data.password,
+    });
+
+    if (userOrError.isFailure) {
+      return left(Result.fail<string>(`${userOrError.getErrorValue()}`));
     }
 
-    private async createUser(data: UserData): Promise<Either<Result<string>, Result<void>>> {
-        try {
-            const userRegisteredByAdmin = await this.repository.get({ ci: data.ci.value });
+    const user: User = userOrError.getValue();
 
-            if (!userRegisteredByAdmin.email == null) {
-                return left(Result.fail<string>(`CI: ${data.ci.value} ya se encuentra registrada`))
-            }
-            // update user
-            userRegisteredByAdmin.editUser(
-                userRegisteredByAdmin.firstName,
-                userRegisteredByAdmin.lastName,
-                data.email,
-                data.ci,
-            )
+    await this.repository.save(user);
 
-            userRegisteredByAdmin.changePassword(data.password);
+    return right(Result.ok<void>());
+  }
 
-            await this.repository.save(userRegisteredByAdmin);
-            return right(Result.ok<void>());
+  private async createTracker(
+    data: CreateTracker,
+  ): Promise<Either<Result<string>, Result<void>>> {
+    const userOrError = User.create({
+      firstName: data.firstName,
+      lastName: data.lastName,
+      email: data.email,
+      password: data.password,
+      canTrack: data.canTrack,
+    });
 
-        } catch (e) { }
-
-        const userOrError = User.create({
-            firstName: data.firstName,
-            lastName: data.lastName,
-            ci: data.ci,
-            email: data.email,
-            password: data.password,
-        });
-
-        if (userOrError.isFailure) {
-            return left(
-                Result.fail<string>(`${userOrError.getErrorValue()}`)
-            )
-        }
-
-        const user: User = userOrError.getValue();
-
-        await this.repository.save(user);
-
-        return right(Result.ok<void>());
+    if (userOrError.isFailure) {
+      return left(Result.fail<string>(`${userOrError.getErrorValue()}`));
     }
 
-    private async createTracker(data: CreateTracker): Promise<Either<Result<string>, Result<void>>> {
-        const userOrError = User.create({
-            firstName: data.firstName,
-            lastName: data.lastName,
-            email: data.email,
-            password: data.password,
-            canTrack: data.canTrack,
-        });
+    const user: User = userOrError.getValue();
 
-        if (userOrError.isFailure) {
-            return left(
-                Result.fail<string>(`${userOrError.getErrorValue()}`)
-            )
-        }
+    await this.repository.save(user);
 
-        const user: User = userOrError.getValue();
+    return right(Result.ok<void>());
+  }
 
-        await this.repository.save(user);
+  async execute(request: CreateUserDto): Promise<Response> {
+    const firstNameOrError = Name.create({ value: request.firstName });
+    const lastNameOrError = Name.create({ value: request.lastName });
+    const emailOrError = UserEmail.create(request.email);
+    const passwordOrError = UserPassword.create({
+      value: request.password,
+    });
 
-        return right(Result.ok<void>());
+    const dtoResult = Result.combine([
+      firstNameOrError,
+      lastNameOrError,
+      emailOrError,
+      passwordOrError,
+    ]);
+
+    if (dtoResult.isFailure) {
+      return left(Result.fail<string>(dtoResult.getErrorValue())) as Response;
     }
 
-    async execute(request: CreateUserDto): Promise<Response> {
-        const firstNameOrError = Name.create({ value: request.firstName });
-        const lastNameOrError = Name.create({ value: request.lastName });
-        const emailOrError = UserEmail.create(request.email);
-        const passwordOrError = UserPassword.create({
-            value: request.password,
-        });
+    const firstName: Name = firstNameOrError.getValue();
+    const lastName: Name = lastNameOrError.getValue();
+    const email: UserEmail = emailOrError.getValue();
+    const password: UserPassword = passwordOrError.getValue();
 
-        const dtoResult = Result.combine([
-            firstNameOrError,
-            lastNameOrError,
-            emailOrError,
-            passwordOrError,
-        ]);
+    const emailAlreadyTaken = await this.repository.exists(email);
 
-        if (dtoResult.isFailure) {
-            return left(
-                Result.fail<string>(dtoResult.getErrorValue())
-            ) as Response;
-        }
-
-        const firstName: Name = firstNameOrError.getValue();
-        const lastName: Name = lastNameOrError.getValue();
-        const email: UserEmail = emailOrError.getValue();
-        const password: UserPassword = passwordOrError.getValue();
-
-        const emailAlreadyTaken = await this.repository.exists(email);
-
-        if (emailAlreadyTaken) {
-            return left(
-                new CreateUserErrors.EmailAlreadyExistsError(email.value)
-            );
-        }
-
-        if (request.canTrack) {
-            return this.createTracker({
-                firstName,
-                lastName,
-                email,
-                password,
-                canTrack: request.canTrack,
-            });
-        }
-
-        const ciOrError = CI.create({ value: request.ci! });
-
-        if (ciOrError.isFailure) {
-            return left(Result.fail<string>(`${ciOrError.getErrorValue()}`));
-        }
-
-        const ci = ciOrError.getValue();
-
-        return await this.createUser({
-            firstName,
-            lastName,
-            email,
-            password,
-            ci
-        })
+    if (emailAlreadyTaken) {
+      return left(new CreateUserErrors.EmailAlreadyExistsError(email.value));
     }
+
+    if (request.canTrack) {
+      return this.createTracker({
+        firstName,
+        lastName,
+        email,
+        password,
+        canTrack: request.canTrack,
+      });
+    }
+
+    const ciOrError = CI.create({ value: request.ci! });
+
+    if (ciOrError.isFailure) {
+      return left(Result.fail<string>(`${ciOrError.getErrorValue()}`));
+    }
+
+    const ci = ciOrError.getValue();
+
+    return await this.createUser({
+      firstName,
+      lastName,
+      email,
+      password,
+      ci,
+    });
+  }
 }
