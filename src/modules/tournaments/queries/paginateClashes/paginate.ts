67554136acb7d5d4bd9fa512b128d55d86106e27
Andre Izarra <programador385@gmail.com>
2025-05-31 20:19:37 -0400

    Hardcode limit pagination in tournaments clases
    This is just for a problem with the pagination in the app and the
    necesity for fix the problem without update the app version

--- src/modules/tournaments/queries/paginateClashes/paginate.ts
+++ src/modules/tournaments/queries/paginateClashes/paginate.ts
@@ -3,53 +3,53 @@ import { Either, Result, left, right } from "../../../../shared/core/Result";
 import { UseCase } from "../../../../shared/core/UseCase";
 import { PaginateResponse } from "../../../../shared/infra/database/sequelize/queries/sequelizeQueries";
 import {
-    ContestClashQuery,
-    ContestClashRepository,
+	ContestClashQuery,
+	ContestClashRepository,
 } from "../../repository/contestClashRepo";
 
 type Req = {
-    limit?: number;
-    offset?: number;
-    contestId: string;
+	limit?: number;
+	offset?: number;
+	contestId: string;
 };
 
 type Res = Either<
-    AppError.UnexpectedError | AppError.NotFoundError,
-    Result<PaginateResponse<any>>
+	AppError.UnexpectedError | AppError.NotFoundError,
+	Result<PaginateResponse<any>>
 >;
 
 export class PaginateClashes implements UseCase<Req, Res> {
-    private readonly clashRepo: ContestClashRepository;
+	private readonly clashRepo: ContestClashRepository;
 
-    constructor(clashRepo: ContestClashRepository) {
-        this.clashRepo = clashRepo;
-    }
+	constructor(clashRepo: ContestClashRepository) {
+		this.clashRepo = clashRepo;
+	}
 
-    async execute(request: Req): Promise<Res> {
-        try {
-            const query: ContestClashQuery = {};
+	async execute(request: Req): Promise<Res> {
+		try {
+			const query: ContestClashQuery = {};
 
-            const validFilters: Array<keyof ContestClashQuery> = [
-                "deep",
-                "team1Id",
-                "team2Id",
-                "contestId",
-            ];
+			const validFilters: Array<keyof ContestClashQuery> = [
+				"deep",
+				"team1Id",
+				"team2Id",
+				"contestId",
+			];
 
-            for (const [k, v] of Object.entries(request)) {
-                if (validFilters.includes(k as keyof ContestClashQuery)) {
-                    query[k as keyof ContestClashQuery] = v as any;
-                }
-            }
+			for (const [k, v] of Object.entries(request)) {
+				if (validFilters.includes(k as keyof ContestClashQuery)) {
+					query[k as keyof ContestClashQuery] = v as any;
+				}
+			}
 
-            const result = await this.clashRepo.paginate(query, {
-                offset: request.offset,
-                limit: request.limit,
-            });
+			const result = await this.clashRepo.paginate(query, {
+				offset: request.offset,
+				limit: 99,
+			});
 
-            return right(Result.ok(result));
-        } catch (error) {
-            return left(new AppError.UnexpectedError(error));
-        }
-    }
+			return right(Result.ok(result));
+		} catch (error) {
+			return left(new AppError.UnexpectedError(error));
+		}
+	}
 }
